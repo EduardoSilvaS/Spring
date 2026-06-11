98fc606a1cad4e9dbea9888a1281808fb8d087de
eduardosilvas <eduardoss2020.ve@gmail.com>
2026-06-11 14:39:31 -0300

    fazendo umas paradas ai

---
+++ src/main/java/com/ciandt/eduardoss/springteste/HomeController.java
@@ -0,0 +1,13 @@
+package com.ciandt.eduardoss.springteste;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
